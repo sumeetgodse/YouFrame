17085e6d1c92089acf8a202f8172e37bb9c63856
Sumeet Godse <sumeetgodse1997@gmail.com>
2020-11-26 15:36:16 +0530

    Add files via upload

--- youframe.py
+++ youframe.py
@@ -1,33 +1,29 @@
 import requests,os,re
 from flask import Flask,redirect,url_for,render_template,request
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename           # Used to store filename
 
 app=Flask(__name__)
 
 @app.route("/")
-def index():
-        return render_template("index.html")
-
-@app.route("/uploader")
 def uploader():
         path = 'static/uploads/'
-        uploads = sorted(os.listdir(path), key=lambda x: os.path.getctime(path+x))
+        uploads = sorted(os.listdir(path), key=lambda x: os.path.getctime(path+x))        # Sorting as per image upload date and time
         print(uploads)
         #uploads = os.listdir('static/uploads')
         uploads = ['uploads/' + file for file in uploads]
         uploads.reverse()
-        return render_template("index.html",uploads=uploads)
+        return render_template("index.html",uploads=uploads)            # Pass filenames to front end for display in 'uploads' variable
 
-app.config['UPLOAD_PATH'] = 'static/uploads'
+app.config['UPLOAD_PATH'] = 'static/uploads'             # Storage path    
 @app.route("/upload",methods=['GET','POST'])
-def upload_file():
+def upload_file():                                       # This method is used to upload files 
         if request.method == 'POST':
                 f = request.files['file']
                 print(f.filename)
                 #f.save(secure_filename(f.filename))
                 filename = secure_filename(f.filename)
                 f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-                return redirect("/uploader")
+                return redirect("/")           # Redirect to route '/' for displaying images on fromt end
 
 if __name__=="__main__":
     app.run()
